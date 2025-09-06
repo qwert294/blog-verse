2d333dace8551968da3095dea401ec898dae6559
qwert294 <mohitreddipalli06@gmail.com>
2025-09-06 15:54:15 +0530

    t

---
+++ functions.js
@@ -0,0 +1,27 @@
+function greet(){
+    console.log("welcome to welcome ")
+}
+greet()
+function add(x,y,){
+    return x+y;
+}
+let result = add(18,20)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+
+const multiply = (x,y)=>{
+    return x*y;
+}
+
+console.log(multiply(5,3))
+
+//arrow function
+const square = (x)=>x*x;
+console.log(square(4))
+const substract = (x,y)=>{
+    return x-y;
+}
+console.log(substract(20,15))
+
