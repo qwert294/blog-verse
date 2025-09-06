303e82a47f4477645c298d8bb70316b6939ed0eb
qwert294 <mohitreddipalli06@gmail.com>
2025-09-06 12:34:30 +0530

    they dont really care about us

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log("Hello World , Krishna")
+
+let name = "Micheal Jackson";
+console.log(name)
+
+let num = 18
+console.log(num)
+
+let istlogged= false;
+console.log(istlogged)
+
+let x;
+console.log(x)
+
+let y=null;
+console.log(y)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof istlogged)
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof arr)
+
+let person= {
+    name:"villain",
+    age:19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
