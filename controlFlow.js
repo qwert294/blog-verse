7472765e4c0b6c556b2fa3c14718391781537340
qwert294 <mohitreddipalli06@gmail.com>
2025-09-06 15:35:24 +0530

    they

---
+++ controlFlow.js
@@ -0,0 +1,51 @@
+// let marks=45;
+// if(marks>=90){
+//     console.log("A+ grade")
+
+// }else if(marks>=80){
+//     console.log("A")
+// }else if(marks>=70){
+//     console.log("B")
+// }else{
+//     console.log("fail")
+// }
+ 
+// let day=3
+// switch(day){
+//     case 1: console.log("monday")
+//     break;
+//     case 2: console.log("tuesday")
+//     break;
+//     case 3: console.log("wednesday")
+//     break;
+//     default: console.log("empty")    
+// }
+
+// let k=1;
+// do{
+//     console.log("iteration:",k);
+//     k++;
+
+// }while(k<=5);
+// let colors=["red","blue","green"]
+// for(let i=0;i<colors.length;i++){
+//     console.log(colors[i])
+// }
+
+let person= {
+    name:"wer",
+    age:19,
+    college:"JNTUGV"
+}
+for(let key in person){
+    console.log(`${key}:${person[key]}`)
+}
+for(let i=5;i>0;i--){
+    if(i==3){
+        continue;
+
+    }else if(i==2){
+        break;
+    }
+    console.log(i)
+}
---
+++ operators.js
@@ -0,0 +1,67 @@
+//Arthemetic operators
+// let x=20,y=3;
+
+// console.log(x+y)
+// console.log(x-y)
+// console.log(x*y)
+// console.log(x/y)
+// console.log(x%y)
+// console.log(x**y)
+
+// //Assignment operators 
+
+// let z=53;
+// z+=3;
+// console.log(z)
+// z*=2;
+// console.log(z)
+// z-=4;
+// console.log(z)
+// z/=2;
+// console.log(z)
+// z%=2;
+// console.log(z)
+
+// //comparision operators
+// let a=10,b="18";
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(a===b)
+// console.log(a!==b)
+// console.log(a>=b)
+// console.log(a<=b)
+
+// //logical operator
+// let age=20;
+// console.log(age>=20 && age<=40)
+// console.log(age<20||age>20)
+// console.log(!(age<18))
+
+// //string operators
+// let firstName= "names"
+// let lastName= "last "
+// let fullName=firstName+" "+lastName;
+
+// console.log(fullName)
+// console.log(lastName)
+// let correctName=`Hello ${firstName} ${lastName}, welcome to ....`;
+// console.log(correctName)
+
+// //ternary operators 
+// let myAge= 19;
+// if(myAge>=18){
+//     console.log("Eligible to vote")
+// }else{
+//     console.log("you cannot vote")
+// }
+// let result= myAge>=18?"eligible to vote":"you cannot vote"
+// console.log(result)
+
+//Increment operator
+let count=5;
+console.log(count++)
+console.log(count)
+console.log(++count)
+
+// typeof operators 
+console.log(typeof count)
